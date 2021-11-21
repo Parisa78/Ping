f80c06034e650c4c71a3527e74890b5d793b8bd8
Parisa78 <parisa_alaie@comp.iust.ac.ir>
2021-11-21 17:51:08 +0330

    add enemy and player and ball and add move ball and physics and move of enemy

---
+++ Pong/pong/Assets/Scripts/TagNames.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum TagNames
+{
+    leftWall,
+    rightWall,
+    roof,
+    ground,
+    EndBack,
+    EndFront,
+    bullet,
+    enemy,
+    Hbullet,
+    fuel,
+    heart
+}
+    
---
+++ Pong/pong/Assets/Scripts/enemy.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemy : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    private SpriteRenderer spriteRenderer;
+    public bool isMovingSameDirection;
+
+    private void Awake()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    public void Move(Vector3 vec, bool isDirRight)
+    {
+
+        int factor = 1;
+        if (isDirRight)
+        {
+            if (isMovingSameDirection)
+            {
+                spriteRenderer.flipX = false;
+                factor = 1;
+            }
+            else
+            {
+                spriteRenderer.flipX = true;
+                factor = -1;
+            }
+        }
+        else
+        {
+
+            if (isMovingSameDirection)
+            {
+                spriteRenderer.flipX = true;
+                factor = -1;
+            }
+            else
+            {
+                spriteRenderer.flipX = false;
+                factor = 1;
+
+            }
+        }
+
+        transform.position += vec * factor;
+    }
+}
